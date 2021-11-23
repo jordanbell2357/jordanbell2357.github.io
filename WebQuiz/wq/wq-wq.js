de1870ef351594a829c95646b541b013e22f6912
Jordan Bell <47544607+jordanbell2357@users.noreply.github.com>
2021-11-22 20:24:56 -0500

    wq

---
+++ WebQuiz/wq/wq-wq.js
@@ -0,0 +1,8 @@
+QuizSpecifications[1]=[];
+QuizSpecifications[1].type="single";
+QuizSpecifications[1][0]=true;
+QuizSpecifications[1][1]=false;
+QuizSpecifications[1][2]=false;
+QuizSpecifications[1][3]=false;
+initSession();
+gotoQuestion(1);
