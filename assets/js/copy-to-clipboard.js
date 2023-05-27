6b1fc5b4ccaaf10cbf58e40a3ba7a8c38bc9c230
Jordan Bell <jordan.bell@gmail.com>
2023-05-27 15:15:18 -0400

    js

--- assets/js/copy-to-clipboard.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var clipboard = new ClipboardJS('.copy-button');
-
-  clipboard.on('success', function (e) {
-    e.clearSelection();
-    console.log('Copied!');
-  });
-});
